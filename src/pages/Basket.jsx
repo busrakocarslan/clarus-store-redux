50408ca0bd4ef27510fad4d7983b954daf86afaa
busra <kocarslanbusrab@gmail.com>
2024-05-03 21:34:24 +0300

    useSelector

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import FiiledBasketTotal from '../components/FiiledBasketTotal';
-import EmptyBasketTotal from '../components/EmptyBasketTotal';
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import FiiledBasketTotal from "../components/FiiledBasketTotal";
+import EmptyBasketTotal from "../components/EmptyBasketTotal";
 
 const Basket = () => {
   const basketItems = useSelector((state) => state.basket);
+
   return (
     <div>
       {basketItems.length > 0 ? <FiiledBasketTotal /> : <EmptyBasketTotal />}
     </div>
-  )
-}
+  );
+};
 
-export default Basket
+export default Basket;
